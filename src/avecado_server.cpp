01e7be5d624dc15baf49ac524be7f37982065940
Kevin Kreiser <kevinkreiser@gmail.com>
2014-08-18 17:42:28 -0400

    oops we need more than just a tcp socket most likely..

--- src/avecado_server.cpp
+++ src/avecado_server.cpp
@@ -14,101 +14,7 @@
 namespace bpo = boost::program_options;
 using boost::asio::ip::tcp;
 
-class session
-{
-public:
-  session(boost::asio::io_service& io_service)
-    : socket_(io_service)
-  {
-  }
 
-  tcp::socket& socket()
-  {
-    return socket_;
-  }
-
-  void start()
-  {
-    socket_.async_read_some(boost::asio::buffer(data_, max_length),
-        boost::bind(&session::handle_read, this,
-          boost::asio::placeholders::error,
-          boost::asio::placeholders::bytes_transferred));
-  }
-
-private:
-  void handle_read(const boost::system::error_code& error,
-      size_t bytes_transferred)
-  {
-    if (!error)
-    {
-      boost::asio::async_write(socket_,
-          boost::asio::buffer(data_, bytes_transferred),
-          boost::bind(&session::handle_write, this,
-            boost::asio::placeholders::error));
-    }
-    else
-    {
-      delete this;
-    }
-  }
-
-  void handle_write(const boost::system::error_code& error)
-  {
-    if (!error)
-    {
-      socket_.async_read_some(boost::asio::buffer(data_, max_length),
-          boost::bind(&session::handle_read, this,
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
-    }
-    else
-    {
-      delete this;
-    }
-  }
-
-  tcp::socket socket_;
-  enum { max_length = 1024 };
-  char data_[max_length];
-};
-
-class server
-{
-public:
-  server(boost::asio::io_service& io_service, short port)
-    : io_service_(io_service),
-      acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
-  {
-    start_accept();
-  }
-
-private:
-  void start_accept()
-  {
-    session* new_session = new session(io_service_);
-    acceptor_.async_accept(new_session->socket(),
-        boost::bind(&server::handle_accept, this, new_session,
-          boost::asio::placeholders::error));
-  }
-
-  void handle_accept(session* new_session,
-      const boost::system::error_code& error)
-  {
-    if (!error)
-    {
-      new_session->start();
-    }
-    else
-    {
-      delete new_session;
-    }
-
-    start_accept();
-  }
-
-  boost::asio::io_service& io_service_;
-  tcp::acceptor acceptor_;
-};
 
 int main(int argc, char *argv[]) {
   unsigned int path_multiplier;
@@ -222,9 +128,9 @@ int main(int argc, char *argv[]) {
 
   //start up the server
   try {
-    boost::asio::io_service io_service(thread_hint);
-    server s(io_service, atoi(argv[1]));
-    io_service.run();
+    //http server 4 example might be useful we can cut out the file stuff.
+    //other, as yet to be thought of, options may be lighter weight though...
+    //http://www.boost.org/doc/libs/1_53_0/doc/html/boost_asio/examples.html
   }
   catch (std::exception& e)
   {
