046515824805d6b5ae48ac1e4bf16b3c48fb777c
Matt Amos <matt.amos@mapquest.com>
2014-08-20 18:34:21 +0100

    Fix bug: paths have a leading slash.

--- src/http_server/parse_path.cpp
+++ src/http_server/parse_path.cpp
@@ -12,18 +12,24 @@ bool parse_path(const std::string &path, int &z, int &x, int &y)
   std::vector<std::string> splits;
   boost::algorithm::split(splits, path, boost::algorithm::is_any_of("/."));
   
-  if (splits.size() != 4) {
+  // we're expecting a leading /, then 3 numbers separated by /,
+  // then ".pbf" at the end.
+  if (splits.size() != 5) {
     return false;
   }
 
-  if (splits[3] != "pbf") {
+  if (splits[0] != "") {
+    return false;
+  }
+
+  if (splits[4] != "pbf") {
     return false;
   }
 
   try {
-    z = boost::lexical_cast<int>(splits[0]);
-    x = boost::lexical_cast<int>(splits[1]);
-    y = boost::lexical_cast<int>(splits[2]);
+    z = boost::lexical_cast<int>(splits[1]);
+    x = boost::lexical_cast<int>(splits[2]);
+    y = boost::lexical_cast<int>(splits[3]);
 
     return true;
 
