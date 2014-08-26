49aeb4fc90340a10b8fb9c10341c2d03c9f68192
Paul Norman <Paul.Norman@mapquest.com>
2014-08-25 17:09:47 -0700

    Finish framing out what's needed for tests
    Also fix a dos/unix endline issue

--- test/make_vector_tile.cpp
+++ test/make_vector_tile.cpp
@@ -1,5 +1,27 @@
 #include "common.hpp"
 
-int main () {
-  return 0;
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void test_single_point() {
+/* This test creates a CSV source with a single point at 0,0,
+ * a map file which renders that point, and checks the resulting
+ * vector tile for 0/0/0
+ */
+
+}
+
+int main() {
+  int tests_failed = 0;
+
+  cout << "== Testing make_vector_tile ==" << endl << endl;
+
+#define RUN_TEST(x) { tests_failed += test::run(#x, &(x)); }
+
+  cout << " >> Tests failed: " << tests_failed << endl << endl;
+
+  return (tests_failed > 0) ? 1 : 0;
+
 }
