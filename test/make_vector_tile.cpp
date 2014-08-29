ddfc70da93a4e64b8b4134700cd12d8a139c6eb0
Paul Norman <Paul.Norman@mapquest.com>
2014-08-28 20:49:01 -0400

    Test with linestrings and polygons, as well as a linestring at z1

--- test/make_vector_tile.cpp
+++ test/make_vector_tile.cpp
@@ -61,9 +61,18 @@ std::string feature_to_geojson(mapnik::feature_impl const& feature)
   return json;
 }
 
+/* JSON strings for map layers. These have a loss of precisision from the
+ * vector tile conversion, and the best way to generate them is just to take
+ * the actual output and then check that it's sensible */
+const std::string single_point_json (R"({"type":"Feature","id":1,"geometry":{"type":"Point","coordinates":[0,0]},"properties":{"name":"null island"}})");
+const std::string single_line_json (R"({"type":"Feature","id":1,"geometry":{"type":"LineString","coordinates":[[-2035059.44106453,0],[-939258.203568246,1252344.27142433],[939258.203568246,939258.203568246],[2035059.44106453,-0.0000000001164153]]},"properties":{"name":"null highway"}})");
+const std::string single_poly_json (R"({"type":"Feature","id":1,"geometry":{"type":"Polygon","coordinates":[[[-2035059.44106453,0],[-939258.203568246,1095801.23749629],[939258.203568246,939258.203568246],[2035059.44106453,0],[-2035059.44106453,0],[-2035059.44106453,0]],[[-156543.033928041,0],[0.0000000000873115,156543.033928041],[156543.033928041,0],[-156543.033928041,0],[-156543.033928041,0]]]},"properties":{"name":"null lake"}})");
+// intersected with a z1 tile
+const std::string single_line_z1_json (R"({"type":"Feature","id":1,"geometry":{"type":"LineString","coordinates":[[-2035059.44106453,0],[-1017529.72053227,1252344.27142433],[-0.0000000002328306,1095801.23749629]]},"properties":{"name":"null highway"}})");
+
 void test_single_point() {
-/* This test uses a map file with a CSV source single point at 0,0
- * and checks the resulting vector tile for 0/0/0
+/* This test uses a map file with a CSV source single point at 0,0 and checks
+ * the resulting vector tile at 0/0/0
  */
   mapnik::Map map;
   avecado::tile tile;
@@ -88,10 +97,100 @@ void test_single_point() {
   fs = ds.features(qq);
   mapnik::feature_ptr feat = fs->next();
   std::string json = feature_to_geojson(*feat);
-  test::assert_equal(json,
-                     std::string(R"({"type":"Feature","id":1,"geometry":{"type":"Point","coordinates":[0,0]},"properties":{"name":"null island"}})"),
-                     "Wrong JSON");
+  test::assert_equal(json, single_point_json, "Wrong JSON");
 }
+
+void test_single_line() {
+/* This test uses a map file with a CSV source line and checks the resulting
+ * vector tile at 0/0/0
+ */
+  mapnik::Map map;
+  avecado::tile tile;
+  // to-do: can we do this completely inline?
+  mapnik::load_map(map, "test/single_line.xml");
+  map.resize(tile_size, tile_size);
+  map.zoom_to_box(bbox);
+  avecado::make_vector_tile(tile, path_multiplier, map, buffer_size, scale_factor,
+                            offset_x, offset_y, tolerance, image_format,
+                            scaling_method, scale_denominator);
+  mapnik::vector::tile result;
+  result.ParseFromString(tile.get_data());
+  test::assert_equal(result.layers_size(), 1, "Wrong number of layers");
+  mapnik::vector::tile_layer layer = result.layers(0);
+
+  // Query the layer with mapnik. See https://github.com/mapbox/mapnik-vector-tile/blob/2e3e2c28/test/vector_tile.cpp#L236
+  mapnik::vector::tile_datasource ds(layer, 0, 0, 0, tile_size);
+
+  mapnik::query qq = mapnik::query(bbox);
+  qq.add_property_name("name");
+  mapnik::featureset_ptr fs;
+  fs = ds.features(qq);
+  mapnik::feature_ptr feat = fs->next();
+  std::string json = feature_to_geojson(*feat);
+  test::assert_equal(json, single_line_json, "Wrong JSON");
+}
+
+void test_single_polygon() {
+/* This test uses a map file with a CSV source polygon and checks the
+ * resulting vector tile at 0/0/0
+ */
+  mapnik::Map map;
+  avecado::tile tile;
+  // to-do: can we do this completely inline?
+  mapnik::load_map(map, "test/single_poly.xml");
+  map.resize(tile_size, tile_size);
+  map.zoom_to_box(bbox);
+  avecado::make_vector_tile(tile, path_multiplier, map, buffer_size, scale_factor,
+                            offset_x, offset_y, tolerance, image_format,
+                            scaling_method, scale_denominator);
+  mapnik::vector::tile result;
+  result.ParseFromString(tile.get_data());
+  test::assert_equal(result.layers_size(), 1, "Wrong number of layers");
+  mapnik::vector::tile_layer layer = result.layers(0);
+
+  // Query the layer with mapnik. See https://github.com/mapbox/mapnik-vector-tile/blob/2e3e2c28/test/vector_tile.cpp#L236
+  mapnik::vector::tile_datasource ds(layer, 0, 0, 0, tile_size);
+
+  mapnik::query qq = mapnik::query(bbox);
+  qq.add_property_name("name");
+  mapnik::featureset_ptr fs;
+  fs = ds.features(qq);
+  mapnik::feature_ptr feat = fs->next();
+  std::string json = feature_to_geojson(*feat);
+  test::assert_equal(json, single_poly_json, "Wrong JSON");
+}
+
+void test_intersected_line() {
+/* This test uses a map file with a CSV source line and checks the resulting
+ * vector tile for 1/0/0, while the line extends outside that tile
+ */
+  mapnik::Map map;
+  avecado::tile tile;
+  // to-do: can we do this completely inline?
+  mapnik::load_map(map, "test/single_line.xml");
+  map.resize(tile_size, tile_size);
+  map.zoom_to_box(box_for_tile(1, 0, 0));
+  avecado::make_vector_tile(tile, path_multiplier, map, buffer_size, scale_factor,
+                            offset_x, offset_y, tolerance, image_format,
+                            scaling_method, scale_denominator);
+  mapnik::vector::tile result;
+  result.ParseFromString(tile.get_data());
+  test::assert_equal(result.layers_size(), 1, "Wrong number of layers");
+  mapnik::vector::tile_layer layer = result.layers(0);
+
+  // Query the layer with mapnik. See https://github.com/mapbox/mapnik-vector-tile/blob/2e3e2c28/test/vector_tile.cpp#L236
+  // note tile_datasource has x, y, z
+  mapnik::vector::tile_datasource ds(layer, 0, 0, 1, tile_size);
+
+  mapnik::query qq = mapnik::query(box_for_tile(1, 0, 0));
+  qq.add_property_name("name");
+  mapnik::featureset_ptr fs;
+  fs = ds.features(qq);
+  mapnik::feature_ptr feat = fs->next();
+  std::string json = feature_to_geojson(*feat);
+  test::assert_equal(json, single_line_z1_json, "Wrong JSON");
+}
+
 
 int main() {
   int tests_failed = 0;
@@ -101,6 +200,9 @@ int main() {
 #define RUN_TEST(x) { tests_failed += test::run(#x, &(x)); }
   RUN_TEST(setup_mapnik);
   RUN_TEST(test_single_point);
+  RUN_TEST(test_single_line);
+  RUN_TEST(test_single_polygon);
+  RUN_TEST(test_intersected_line);
   cout << " >> Tests failed: " << tests_failed << endl << endl;
 
   return (tests_failed > 0) ? 1 : 0;
