cb3f786d13e90a906a49596d2c174cf6ccb1e01c
Matt Amos <matt.amos@mapquest.com>
2014-09-02 19:31:46 +0100

    Added converter to use python dict as boost::property_tree::ptree.

--- src/python_module.cpp
+++ src/python_module.cpp
@@ -2,7 +2,9 @@
 
 #include "avecado.hpp"
 #include "post_processor.hpp"
+#include "../logging/logger.hpp"
 
+namespace pt = boost::property_tree;
 using namespace boost::python;
 
 namespace {
@@ -45,12 +47,138 @@ str mk_tile(object py_map,
   return str(buffer.data(), buffer.size());
 }
 
-void pp_load(avecado::post_processor &pp, dict config) {
+/**
+ * this converts Python "dict" objects into boost::property_tree objects
+ * and registers the converter to be available for all methods - makes it
+ * much easier to expose C++ (ptree-using) code to Python-land.
+ *
+ * note: currently only a few types of values (in try_set_all) are
+ * implemented, but this should be easy to add more to.
+ */
+struct boost_ptree_from_python_dict
+{
+  static void *convertible(PyObject *ptr) {
+    return PyMapping_Check(ptr) ? ptr : NULL;
+  }
+
+  static bool try_set_all(pt::ptree &c, const std::string &key, object &val);
+
+  template <typename T>
+  static bool try_set(pt::ptree &c, const std::string &key, object &val);
+
+  static void construct_rec(pt::ptree &c, const dict &d);
+
+  static void construct(PyObject *ptr, converter::rvalue_from_python_stage1_data* data) {
+    object obj(handle<>(borrowed(ptr)));
+    dict d = extract<dict>(obj);
+
+    void *storage = ((converter::rvalue_from_python_storage<pt::ptree>*)
+                     data)->storage.bytes;
+    pt::ptree *tree_storage = new (storage) pt::ptree;
+
+    construct_rec(*tree_storage, d);
+
+    data->convertible = storage;
+  }
+
+  boost_ptree_from_python_dict() {
+    boost::python::converter::registry::push_back(
+      &convertible, &construct, boost::python::type_id<pt::ptree>());
+  }
+};
+
+template <typename T>
+bool boost_ptree_from_python_dict::try_set(pt::ptree &c, const std::string &key, object &val)
+{
+  extract<T> ex(val);
+  if (ex.check()) {
+    c.put<T>(key, ex());
+    return true;
+  }
+  return false;
+}
+
+template <>
+bool boost_ptree_from_python_dict::try_set<pt::ptree>(pt::ptree &c, const std::string &key, object &val) {
+  // first, check if it's a dictionary of key-value pairs
+  {
+    extract<dict> ex(val);
+    if (ex.check()) {
+      // if the key is empty, this is part of a list
+      if (key.empty()) {
+        construct_rec(c, ex());
+      } else {
+        pt::ptree child;
+        construct_rec(child, ex());
+        c.put_child(key, child);
+      }
+      return true;
+    }
+  }
+
+  // if not, then it might be a list of values
+  {
+    extract<list> ex(val);
+    if (ex.check()) {
+      const std::string empty_key;
+      pt::ptree child;
+      list l = ex();
+      const boost::python::ssize_t n = len(l);
+
+      for (boost::python::ssize_t i = 0; i < n; ++i) {
+        pt::ptree grandchild;
+        object obj = l[i];
+        if (try_set_all(grandchild, empty_key, obj)) {
+          child.push_back(std::make_pair(empty_key, grandchild));
+        }
+      }
+
+      c.put_child(key, child);
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool boost_ptree_from_python_dict::try_set_all(pt::ptree &c, const std::string &key, object &val) {
+  bool success = true;
+
+  if (!val.is_none()) {
+    success = (try_set<std::string>(c, key, val) ||
+               try_set<int>(c, key, val) ||
+               try_set<double>(c, key, val) ||
+               try_set<pt::ptree>(c, key, val));
+
+    if (!success) {
+      LOG_WARNING(boost::format("Unable to set key=\"%1%\", value=\"%2%\"")
+                  % key % call_method<std::string>(val.ptr(), "__repr__"));
+    }
+  }
+
+  return success;
+}
+
+void boost_ptree_from_python_dict::construct_rec(pt::ptree &c, const dict &d) {
+  list keys=d.keys();
+
+  for (int i = 0; i < len(keys); ++i) {
+    std::string key = extract<std::string>(keys[i]);
+    object val = d[key];
+
+    try_set_all(c, key, val);
+  }
+}
+
+void pp_load(avecado::post_processor &pp, const pt::ptree &conf) {
+  pp.load(conf);
 }
 
 } // anonymous namespace
 
 BOOST_PYTHON_MODULE(avecado) {
+  boost_ptree_from_python_dict dict_converter;
+
   class_<avecado::post_processor, bases<>, 
          boost::shared_ptr<avecado::post_processor>, 
          boost::noncopyable>("PostProcessor")
