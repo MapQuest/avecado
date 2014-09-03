7a33e4ec49f3f6fd351664378bf66cb8161bf05a
Kevin Kreiser <kevinkreiser@gmail.com>
2014-09-03 10:40:57 -0400

    make use of simplify_converter, noting that it doesnt implement peucker at present

--- src/post_process/generalizer.cpp
+++ src/post_process/generalizer.cpp
@@ -1,5 +1,6 @@
 #include "post_process/generalizer.hpp"
 
+#include <vector>
 #include <string>
 #include <mapnik/simplify_converter.hpp>
 
@@ -20,17 +21,43 @@ public:
   virtual void process(vector<mapnik::feature_ptr> &layer) const;
 
 private:
-  //TODO: mapnik simplify_converter shared ptr
+
+  boost::optional<mapnik::simplify_algorithm_e> m_algorithm;
+  double m_tolerance;
 };
 
-generalizer::generalizer(const string& algorithm, const double& tolerance) {
-  //TODO: make a mapnik simplfy_converter shared ptr passing in the algorithm type
-  boost::optional<mapnik::simplify_algorithm_e> algo = mapnik::simplify_algorithm_from_string(algorithm);
+generalizer::generalizer(const string& algorithm, const double& tolerance):
+  m_algorithm(mapnik::simplify_algorithm_from_string(algorithm)),
+  m_tolerance(tolerance) {
+
 }
 
 
 void generalizer::process(vector<mapnik::feature_ptr> &layer) const {
-  // TODO: generalize!
+  //for each feature set
+  for(auto& feat : layer) {
+    //for each geometry
+    for(size_t i = 0; i < feat->num_geometries(); ++i) {
+      //grab the geom
+      mapnik::geometry_type& geom = feat->get_geometry(i);
+      //setup the generalization
+      mapnik::simplify_converter<mapnik::geometry_type> generalizer(geom);
+      if(m_algorithm)
+        generalizer.set_simplify_algorithm(*m_algorithm);
+      generalizer.set_simplify_tolerance(m_tolerance);
+      //suck the vertices back out of it
+      mapnik::geometry_type* output = new mapnik::geometry_type();
+      mapnik::CommandType cmd;
+      double x, y;
+      while((cmd = (mapnik::CommandType)generalizer.vertex(&x, &y)) != mapnik::SEG_END)
+      {
+        output->push_vertex(x, y, cmd);
+      }
+      //and put it back, note that this returns a pointer to the old one
+      //but it will get deallocated (auto_ptr) when it goes out of scope
+      feat->paths().replace(i, output);
+    }
+  }
 }
 
 izer_ptr create_generalizer(pt::ptree const& config) {
