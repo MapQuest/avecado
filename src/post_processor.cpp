bd71c6dcabfdb030a730193003fcaa72a1172398
Kevin Kreiser <kevinkreiser@gmail.com>
2014-10-28 12:13:46 -0400

    change over the zoom stuff to scale within the postproccessor

--- src/post_processor.cpp
+++ src/post_processor.cpp
@@ -9,16 +9,23 @@
 
 namespace pt = boost::property_tree;
 
+namespace {
+  const double WORLD_METERS = 40075016.68;
+  double zoom_to_scale(int zoom) {
+    return WORLD_METERS / double(1 << zoom);
+  }
+}
+
 namespace avecado {
 
 typedef std::vector<post_process::izer_ptr> izer_vec_t;
 typedef struct {
-  int minzoom;
-  int maxzoom;
+  int minscale;
+  int maxscale;
   izer_vec_t processes;
-} zoom_range_t;
-typedef std::vector<zoom_range_t> zoom_range_vec_t;
-typedef std::map<std::string, zoom_range_vec_t> layer_map_t;
+} scale_range_t;
+typedef std::vector<scale_range_t> scale_range_vec_t;
+typedef std::map<std::string, scale_range_vec_t> layer_map_t;
 
 class post_processor::pimpl {
 public:
@@ -26,7 +33,7 @@ public:
   void load(pt::ptree const& config);
   void process_layer(std::vector<mapnik::feature_ptr> & layer,
                      const std::string &layer_name,
-                     int zoom_level) const;
+                     double scale) const;
 private:
   layer_map_t m_layer_processes;
 };
@@ -43,33 +50,33 @@ void post_processor::pimpl::load(pt::ptree const& config) {
          .register_type("unionizer", post_process::create_unionizer);
 
   for (auto layer_child : config) {
-    zoom_range_vec_t zoom_ranges;
+    scale_range_vec_t scale_ranges;
     for (auto range_child : layer_child.second) {
-      zoom_range_t zoom_range;
-      zoom_range.minzoom = range_child.second.get<int>("minzoom");
-      zoom_range.maxzoom = range_child.second.get<int>("maxzoom");
+      scale_range_t scale_range;
+      scale_range.minscale = zoom_to_scale(range_child.second.get<int>("minzoom"));
+      scale_range.maxscale = zoom_to_scale(range_child.second.get<int>("maxzoom"));
       pt::ptree const& process_config = range_child.second.get_child("process");
       for (auto izer_child : process_config) {
         std::string const& type = izer_child.second.get<std::string>("type");
         post_process::izer_ptr p = factory.create(type, izer_child.second);
-        zoom_range.processes.push_back(p);
+        scale_range.processes.push_back(p);
       }
-      zoom_ranges.push_back(std::move(zoom_range));
+      scale_ranges.push_back(std::move(scale_range));
     }
-    m_layer_processes.insert(std::make_pair(layer_child.first, std::move(zoom_ranges)));
+    m_layer_processes.insert(std::make_pair(layer_child.first, std::move(scale_ranges)));
   }
 }
 
-// Find post-processes for given layer at zoom level and run them
+// Find post-processes for given layer at scale level and run them
 void post_processor::pimpl::process_layer(std::vector<mapnik::feature_ptr> & layer,
                                           const std::string &layer_name,
-                                          int zoom_level) const {
+                                          double scale) const {
   layer_map_t::const_iterator layer_itr = m_layer_processes.find(layer_name);
   if (layer_itr != m_layer_processes.end()) {
-    zoom_range_vec_t const& zoom_ranges = layer_itr->second;
-    // TODO: Consider ways to optimize zoom range look up
-    for (auto range : zoom_ranges) {
-      if (zoom_level >= range.minzoom && zoom_level <= range.maxzoom) {
+    scale_range_vec_t const& scale_ranges = layer_itr->second;
+    // TODO: Consider ways to optimize scale range look up
+    for (auto range : scale_ranges) {
+      if (scale >= range.minscale && scale <= range.maxscale) {
         // TODO: unserialize geometry objects to pass through izers
         for (auto p : range.processes) {
           p->process(layer);
@@ -95,8 +102,8 @@ void post_processor::load(pt::ptree const& config) {
 
 void post_processor::process_layer(std::vector<mapnik::feature_ptr> &layer, 
                                    const std::string &layer_name,
-                                   int zoom_level) const {
-  m_impl->process_layer(layer, layer_name, zoom_level);
+                                   double scale) const {
+  m_impl->process_layer(layer, layer_name, scale);
 }
 
 } // namespace avecado
