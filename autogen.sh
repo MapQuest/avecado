1972605a5d0ccee007bc7830c25a6aa448fcfd42
Matt Amos <zerebubuth@gmail.com>
2015-05-08 14:12:47 -0400

    Add submodules warning.
    Check if submodules are initialised before running the autotools
    setup. This helps catch the common error, when first setting up the
    project, of forgetting to init the submodules, which causes a
    pretty incomprehensible error message at a later stage.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+set -e
+
+git submodule status | cut -c 1-1 \
+    | xargs -i% test % != "-" || \
+    ( echo "missing submodules, try: git submodule update --init --recursive" ; exit 1)
+
+libtoolize
 aclocal -I m4
-autoheader
-libtoolize -i
-automake -a
-autoconf
+autoreconf --force --install
+
+echo "Ready to run ./configure"
